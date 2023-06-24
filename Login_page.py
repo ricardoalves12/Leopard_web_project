fe44327ac80e90a70c6f148f695cb734f3ed1b97
ricardoalves12 <alvesalmeidamoreirar@wit.edu>
2023-06-24 18:22:45 -0400

    Gui implemented

--- Login_page.py
+++ Login_page.py
@@ -1,9 +1,11 @@
 from tkinter import *
-import tkinter as tk
+import tkinter 
+#pip install pillow
+from PIL import ImageTk, Image 
 from tkinter import PhotoImage, messagebox
 
 
-class MainPage(tk.Tk):
+class MainPage(tkinter.Tk):
     def __init__(self):
         super().__init__()
         self.title("Login") 
@@ -12,8 +14,27 @@ class MainPage(tk.Tk):
         height_screen= self.winfo_screenheight()
         self.geometry("%dx%d" % (width_screen, height_screen))
         self.iconphoto(False, PhotoImage(file = 'Images_for_Gui/images.png'))
-        self.login_frame = LoginFrame(self, width = 350, height = 500, bg="white")
+        # Define image
+        bg = ImageTk.PhotoImage(file="Images_for_Gui\wit-background.png")
+        #Create canvas
+        my_canvas = Canvas(self, width = width_screen, height = height_screen)
+        my_canvas.pack(fill="both", expand=True)
+        #set image in canvas
+        my_canvas.create_image(0,0,image=bg, anchor="nw")
+        self.tkinter.Label(self, image= bg).place(x=0, y=0,relwidth=1, relheight=1)
+        self.bind('<Return>', self.resizer)
+        self.login_frame = LoginFrame(self)
         self.ShowLoginFrame()
+    def resizer(self, e):
+        global bg1, resized_bg, new_bg
+        # Open Image
+        bg1= Image.open("Images_for_Gui\wit-background.png")
+        #resize Image
+        resized_bg = bg1.resize((e.width, e.height), Image.ANTIALIAS)
+        #define the image
+        new_bg = ImageTk.PhotoImage(resized_bg)
+        #add it back to canvas
+        self.my_canvas.create_image(0,0, image=new_bg, anchor='nw')
 
     def ShowLoginFrame(self):
         #setting page geometry to the size of the user's screen
@@ -21,24 +42,24 @@ class MainPage(tk.Tk):
         height_screen= self.winfo_screenheight()
         self.login_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
 
-class LoginFrame (tk.Frame):
+class LoginFrame (tkinter.Frame):
     def __init__(self, master):
-        super().__init__(master)
+        super().__init__(master, width = 350, height = 500, bg="white")
         
-        self.label = tk.Label(self, text="Enter Username & Password", font=('Times',14), bg="white")
+        self.label = tkinter.Label(self, text="Enter Username & Password", font=('Times',14), bg="white")
         self.label.place(x=20, y=40)
         
-        self.username_label = tk.Label(self, text="Username:", font=('Times',12), bg="white")
+        self.username_label = tkinter.Label(self, text="Username:", font=('Times',12), bg="white")
         self.username_label.place(x=20, y=80)
-        self.username_entry = tk.Entry(self, highlightbackground='black', highlightthickness=1,bd=0,width=34,font=('Times',14))
+        self.username_entry = tkinter.Entry(self, highlightbackground='black', highlightthickness=1,bd=0,width=34,font=('Times',14))
         self.username_entry.place(x=20, y=110)
         
-        self.password_label = tk.Label(self, text="Password:", font=('Times',12), bg="white")
+        self.password_label = tkinter.Label(self, text="Password:", font=('Times',12), bg="white")
         self.password_label.place(x=20, y=140)
-        self.password_entry = tk.Entry(self, highlightbackground='black', highlightthickness=1,bd=0,width=34,font=('Times',14), show="*")
-        self.password_entry.place(x=20, y=1700)
+        self.password_entry = tkinter.Entry(self, show="*", highlightbackground='black', highlightthickness=1,bd=0,width=34,font=('Times',14))
+        self.password_entry.place(x=20, y=170)
         
-        self.login_button = tk.Button(self, ext="Login", bg="red", fg="white", width=17, font=('Times',24), bd=0, command=self.login)
+        self.login_button = tkinter.Button(self, text="Login", bg="red", fg="white", width=17, font=('Times',24), bd=0, command=self.login)
         self.login_button.place(x=20, y=210)
     def login(self):
         username = self.username_entry.get() 
@@ -51,6 +72,6 @@ class LoginFrame (tk.Frame):
         else:
             messagebox.showerror("Login Failed", "Invalid username or password.")
 
-if __name__ == "main":
+if __name__ == "__main__":
     app = MainPage()
     app.mainloop()
