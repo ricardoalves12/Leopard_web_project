c41c7c941082cfd1fd79b51f3a33a10bcde78d14
ManuelChoute <choutem@wit.edu>
2023-06-26 14:36:31 -0400

    linking code to database

--- Database/Class_Create.py
+++ Database/Class_Create.py
@@ -17,7 +17,8 @@ class Class_Creator:
      self.cursor.execute(""" CREATE TABLE AUTHENTIFY(
         PASSWORD TEXT  NOT NULL,
          USERNAME TEXT NOT NULL,
-         USER_ID INTERGER NOT NULL
+         USER_ID INTERGER NOT NULL,
+         USER_DESCRIPTION TEXT NOT NULL
          PRIMARY KEY 
           );"""
 
@@ -28,10 +29,15 @@ class Class_Creator:
       Value =f"""INSERT INTO  AUTHENTIFY VALUES((?),(?),(?));"""
       self.cursor.execute(Value,(Password,USER,ID))
       self.connect.commit()
+    
+   def Alter(self,col):
+     Val=f""" ALTER TABLE AUTHENTIFY ADD COLUMN {col};"""
+     self.cursor.execute(Val)
+     self.connect.commit()
 
-Database= Class_Creator("Database/tables.db")
+Database= Class_Creator("Leopard_web_project/Database/tables.db")
 Database.Connect()
+Database.Alter("Description")
 
-Database.Insert('MamaGuevo','RicoA',10014)
 Database.Disconnect()
 
