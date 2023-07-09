a807eca9a1cbf3fe4e5453a395ea26c754a66de8
Tommaso0101 <verdiglionet@wit.edu>
2023-07-08 21:28:19 -0400

    instructorn

--- Login_page.py
+++ Login_page.py
@@ -21,6 +21,8 @@ class MainPage(tkinter.Tk):
         self.Instructor_frame = InstructorhomeFrame(self)
         self.Admin_frame = AdminFrame(self)
         self.student_search_class_frame = StudentSearchClassFrame(self)
+        self.instructor_search_class_frame = InstructorSearchClassFrame(self)
+        #self.admin_search_class_frame = AdminSearchClassFrame(self) 
         self.ShowLoginFrame()
 
 
@@ -31,6 +33,7 @@ class MainPage(tkinter.Tk):
         self.login_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
         self.student_frame.place_forget()
         self.student_search_class_frame.place_forget()
+        self.instructor_search_class_frame.place_forget()
         self.Instructor_frame.place_forget()
         self.Admin_frame.place_forget()
 
@@ -40,6 +43,7 @@ class MainPage(tkinter.Tk):
         self.login_frame.place_forget()
         self.student_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
         self.student_search_class_frame.place_forget()
+        self.instructor_search_class_frame.place_forget()
         self.Instructor_frame.place_forget()
         self.Admin_frame.place_forget()
     
@@ -50,24 +54,37 @@ class MainPage(tkinter.Tk):
         self.student_frame.place_forget()
         self.Instructor_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
         self.Admin_frame.place_forget()
+        self.instructor_search_class_frame.place_forget()
         self.student_search_class_frame.place_forget()
-    
+     
     def show_Admin_Frame(self):
         width_screen= self.winfo_screenwidth()
         height_screen= self.winfo_screenheight()
         self.login_frame.place_forget()
         self.student_frame.place_forget()
         self.Admin_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
+        self.instructor_search_class_frame.place_forget()
         self.Instructor_frame.place_forget()
         self.student_search_class_frame.place_forget()
-    
+        
     def show_student_search_Class_Frame(self):
         width_screen= self.winfo_screenwidth()
         height_screen= self.winfo_screenheight()
         self.login_frame.place_forget()
         self.student_frame.place_forget()
         self.student_search_class_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
+        self.instructor_search_class_frame.place_forget()
         self.Instructor_frame.place_forget()
+        self.Admin_frame.place_forget()
+
+    def show_instructor_search_Class_Frame(self):
+        width_screen= self.winfo_screenwidth()
+        height_screen= self.winfo_screenheight()
+        self.login_frame.place_forget()
+        self.student_frame.place_forget()
+        self.student_search_class_frame.place_forget()
+        self.Instructor_frame.place_forget()
+        self.instructor_search_class_frame.place(x=((width_screen/2) -200),y=((height_screen/2) -380))
         self.Admin_frame.place_forget()
 
 class LoginFrame (tkinter.Frame):
@@ -99,12 +116,12 @@ class LoginFrame (tkinter.Frame):
         db.execute("SELECT 1 FROM AUTHENTIFY  WHERE USER_NAME = ? and PASSWORD = ? ", (username, password))
         result = db.fetchone()
         if result:
-            for row in db.execute("SELECT * FROM AUTHENTIFY  WHERE USERNAME = ? and PASSWORD = ? ", (username, password)):
-                if row[3] == 'STUDENT':
+            for row in db.execute("SELECT * FROM AUTHENTIFY  WHERE USER_NAME = ? and PASSWORD = ? ", (username, password)):
+                if row[1] == 'STUDENT':
                     self.master.show_student_Frame()
-                elif row[3] == 'ADMIN':
+                elif row[1] == 'ADMIN':
                     self.master.show_Admin_Frame()
-                elif row[3] == 'INSTRUCTOR':
+                elif row[1] == 'INSTRUCTOR':
                     self.master.show_Instructor_Frame()
                 else:
                     messagebox.showerror("User type failed", "Invalid User type.")
@@ -147,7 +164,7 @@ class InstructorhomeFrame(tkinter.Frame):
         self.label = tkinter.Label(self, text="Instructor Home Page", font=('Times',12), bg="white")
         self.label.place(x=20, y=30)
 
-        self.logout_button = tkinter.Button(self, text="Logout", font=('Times',12),  bg="red", fg="white", bd=0)
+        self.logout_button = tkinter.Button(self, text="Logout", font=('Times',12),  bg="red", fg="white", bd=0, command=self.logout)
         self.logout_button.place(x=285, y=30)
 
         self.PrintSchedule_button = tkinter.Button(self, text="Print Schedule", font=('Times',12),  bg="red", fg="white", bd=0)
@@ -156,11 +173,14 @@ class InstructorhomeFrame(tkinter.Frame):
         self.PrintClassList_button = tkinter.Button(self, text="Print Class List", font=('Times',12),  bg="red", fg="white", bd=0)
         self.PrintClassList_button.place(x=25, y=150)
 
-        self.SearchCourses_button = tkinter.Button(self, text="Search Courses", font=('Times',12),  bg="red", fg="white", bd=0)
+        self.SearchCourses_button = tkinter.Button(self, text="Search Courses", font=('Times',12),  bg="red", fg="white", bd=0, command=self.SearchClass)
         self.SearchCourses_button.place(x=25, y=200)
 
     def logout(self):
         self.master.ShowLoginFrame()
+
+    def SearchClass(self):
+        self.master.show_instructor_search_Class_Frame()
 
 class AdminFrame (tkinter.Frame):
      def __init__(self, master):
@@ -233,6 +253,35 @@ class StudentSearchClassFrame(tkinter.Frame):
         
     def Back(self):
         self.master.show_student_Frame()
+
+class InstructorSearchClassFrame(tkinter.Frame):
+    def __init__(self, master):
+        super().__init__(master, width = 350, height = 500, bg="white")
+        
+        self.label = tkinter.Label(self, text="Search Course Page", font=('Times',12), bg="white")
+        self.label.place(x=20, y=30)
+
+        self.logout_button = tkinter.Button(self, text="Back", font=('Times',12),  bg="red", fg="white", bd=0, command=self.Back)
+        self.logout_button.place(x=285, y=30)
+
+        self.CRN_label = tkinter.Label(self, text="Enter CRN:", font=('Times',12), bg="white")
+        self.CRN_label.place(x=20, y=70)
+        self.CRN_entry = tkinter.Entry(self, highlightbackground='black', highlightthickness=1,bd=0,width=34,font=('Times',14))
+        self.CRN_entry.place(x=20, y=110)
+
+        self.search_button = tkinter.Button(self, text="Search Course", font=('Times',12),  bg="black", fg="white", bd=0, command=self.SearchCourse)
+        self.search_button.place(x=20, y=151)
+               
+    def SearchCourse(self):
+        CRN = self.CRN_entry.get()
+        self.CRN_entry.delete(0, END)
+       
+        # call the Student class to print data base
+        student.Student.search_Course(self,CRN)
+        
+    def Back(self):
+        self.master.show_Instructor_Frame()
+
 
 if __name__ == "__main__":
     app = MainPage()
