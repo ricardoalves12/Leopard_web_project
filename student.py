65d906704d3510d6d63e59629ad2685053700020
ManuelChoute <choutem@wit.edu>
2023-08-03 23:47:02 -0400

    This has been push

--- student.py
+++ student.py
@@ -26,15 +26,49 @@ class Student(User):
             Values=(self.ID,self.first_name,self.last_name,self.graduate,self.Major,self.Email)
             self.cursor.execute(Update,Values)
             self.connect.commit()
-            
 
-            
+       def Check_conflict(self,crn):
+          Value1="""SELECT * FROM COURSE WHERE CRN=?"""
+          self.cursor.execute(Value1,(crn,))
+          Add_Course=self.cursor.fetchall()
+          for C_info in Add_Course:
+            S_DAY=C_info[2]
+            E_DAY=C_info[3]
+            S_TIME=C_info[4]
+            E_TIME=C_info[5]
+
+          Value=""" SELECT * FROM SCHEDULE WHERE ID=?"""  
+          self.cursor.execute(Value,(self.ID,))
+          Check_list=self.cursor.fetchall()
+          Check_List_S_DAY=[]
+          Check_List_E_DAY=[]
+          Check_List_S_TIME=[]
+          Check_List_E_TIME=[]
+         
+          for element in Check_list:
+            Check_List_S_DAY.append(element[4])
+            Check_List_E_DAY.append(element[5])
+            Check_List_S_TIME.append(element[6])
+            Check_List_E_TIME.append(element[7])
+          
+          if S_DAY not in  Check_List_S_DAY:
+            if E_DAY not in Check_List_E_DAY:
+               if S_TIME not in Check_List_S_TIME:
+                if E_TIME not in Check_List_E_TIME:
+                  return True
+          else:
+            return False
+
+             
+         
+         
 
        def Add_Course(self,crn):
           Value="""SELECT * FROM  COURSE WHERE CRN=?"""
           self.cursor.execute(Value,(crn,))
           Schedule=self.cursor.fetchall()
-          for element in Schedule:
+          if self.Check_conflict(crn):
+           for element in Schedule:
             CRN=element[0]
             C_NAME=element[1]
             S_DAY=element[2]
@@ -42,10 +76,13 @@ class Student(User):
             S_TIME=element[4]
             E_TIME=element[5]
             T_NAME=element[6]
-          Value="""INSERT INTO SCHEDULE(ID,ST_NAME,CRN,COURSE,S_DAY,E_DAY,S_TIME,E_TIME,T_NAME) VALUES(?,?,?,?,?,?,?,?,?)"""  
-          Val=(self.ID,self.first_name,CRN,C_NAME,S_DAY,E_DAY,S_TIME,E_TIME,T_NAME)
-          self.cursor.execute(Value,(Val))
-          self.connect.commit()
+
+           Value="""INSERT INTO SCHEDULE(ID,ST_NAME,CRN,COURSE,S_DAY,E_DAY,S_TIME,E_TIME,T_NAME) VALUES(?,?,?,?,?,?,?,?,?)"""  
+           Val=(self.ID,self.first_name,CRN,C_NAME,S_DAY,E_DAY,S_TIME,E_TIME,T_NAME)
+           self.cursor.execute(Value,(Val))
+           self.connect.commit()
+          else:
+            return "There is a time conflict "
 
             
 
